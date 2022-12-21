d0bc586c836e0ab3eb6f35f835a2f37493ad72ae
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2022-12-21 22:46:24 +0700

    #8 create schemas and models of API resources

---
+++ models/movie.js
@@ -0,0 +1,69 @@
+import mongoose from 'mongoose';
+import { linkRegex } from '../validators/common.js';
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value) => linkRegex.test(value),
+      message: (props) => `${props.value} Проверьте корректность ссылки`,
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value) => linkRegex.test(value),
+      message: (props) => `${props.value} Проверьте корректность ссылки`,
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value) => linkRegex.test(value),
+      message: (props) => `${props.value} Проверьте корректность ссылки`,
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+}, { versionKey: false });
+
+export const Movie = mongoose.model('movie', movieSchema);
---
+++ models/user.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import { emailRegex } from '../validators/common.js';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (value) => emailRegex.test(value),
+      message: (props) => `${props.value} Проверьте правильность написания почты`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+}, { versionKey: false });
+
+userSchema.statics.findUserByCredentials = function findUser(email, password) {
+  // ищем пользователя по почте
+  return this.findOne({ email }).select('+password') // this — это модель User
+    .then((document) => {
+      // не нашёлся — отклоняем промис
+      if (!document) {
+        // throw new UnauthorizedError('Неправильные почта или пароль');
+      }
+      // нашёлся — сравниваем хеши
+      return bcrypt.compare(password, document.password)
+        .then((matched) => {
+          if (!matched) {
+            // throw new UnauthorizedError('Неправильные почта или пароль');
+          }
+
+          const user = document.toObject();
+          delete user.password;
+          return user;
+        });
+    });
+};
+
+export const User = mongoose.model('user', userSchema);
